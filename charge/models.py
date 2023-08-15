baf9d326395a2bbfbe1e7123e092fe8956165fc9
sarrkar <sarraah.karimi@gmail.com>
2023-08-15 03:59:07 -0700

    added credit and charge request models

--- charge/models.py
+++ charge/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db import models, transaction
+from django.db.models import F
 
 
 class Seller(models.Model):
@@ -12,3 +13,29 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=11, unique=True)
+
+
+class CreditRequest(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    amount = models.PositiveBigIntegerField()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            with transaction.atomic():
+                self.seller.credit = F('credit') + self.amount
+                self.seller.save()
+                super(CreditRequest, self).save(*args, **kwargs)
+
+
+class ChargeRequest(models.Model):
+
+    STATUS_CHOICES = [
+        ('R', 'REQUESTED'),
+        ('S', 'SUCCESSFUL'),
+        ('F', 'FAILED'),
+    ]
+
+    seller = models.ForeignKey(Seller, null=True, on_delete=models.SET_NULL)
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    amount = models.PositiveBigIntegerField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
